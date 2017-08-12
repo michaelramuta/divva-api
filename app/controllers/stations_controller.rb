5a8cc97c66e6756ac292ea6b91f78ead23d14a8c
Michael Ramuta <michaelramuta@gmail.com>
2017-08-12 00:35:14 -0500

    add station seeding functionality

--- app/controllers/stations_controller.rb
+++ app/controllers/stations_controller.rb
@@ -4,20 +4,25 @@ require 'json'
 class StationsController < ApplicationController
 
 	def index
-    	url = 'https://data.cityofchicago.org/resource/aavc-b2wj.json'
-		uri = URI(url)
-		response = Net::HTTP.get(uri)
-		stations = JSON.parse(response)
+		if params[:password] == ENV['STATION_PASSWORD']
+			url = 'https://data.cityofchicago.org/resource/aavc-b2wj.json'
+			uri = URI(url)
+			response = Net::HTTP.get(uri)
+			stations = JSON.parse(response)
 
-		stations.each do |station|
-			new_station = Station.where(station_id: station['id']).first_or_initialize
-			new_station.lat = station['latitude'].to_f
-			new_station.lon = station['longitude'].to_f
-			new_station.name = station['station_name']
-			new_station.save
+			stations.each do |station|
+				new_station = Station.where(station_id: station['id']).first_or_initialize
+				new_station.lat = station['latitude'].to_f
+				new_station.lon = station['longitude'].to_f
+				new_station.name = station['station_name']
+				new_station.save
+			end
+
+			json_response('good job, stations seeded')
+		else
+			json_response('what are you looking here for?')
 		end
-    	
-		json_response(Station.last)
+
 	end
 end
 
