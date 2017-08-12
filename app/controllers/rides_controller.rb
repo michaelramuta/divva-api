27a69a559acf2695b186ad3ed99b23f3f1c15f6e
Michael Ramuta <michaelramuta@gmail.com>
2017-08-12 01:42:56 -0500

    add distance call to google

--- app/controllers/rides_controller.rb
+++ app/controllers/rides_controller.rb
@@ -1,21 +1,43 @@
+require 'net/http'
+require 'json'
+
 class RidesController < ApplicationController
 
 	def index
 		station_1_id = params[:station_1_id]
 		station_2_id = params[:station_2_id]
+		station_1 = Station.find_by(station_id: station_1_id)
+		station_2 = Station.find_by(station_id: station_2_id)
 
-		if station_1_id != station_2_id && params[:station_1_id].present? && params[:station_2_id].present?
+		if station_1.nil? || station_2.nil?
+			json_response('why')
+		else
+
 			ride = Ride.where(station_1_id: station_1_id, station_2_id: station_2_id).first_or_initialize
 
 			if ride.distance.nil?
-				ride.distance = 1.1111
-				ride.save
-				json_response('updated')
+				base_url = 'https://maps.googleapis.com/maps/api/distancematrix/json'
+				location_parameters = '?origins=' + station_1.lat.to_s + ',' + station_1.lon.to_s + '&destinations=' + station_2.lat.to_s + ',' + station_2.lon.to_s
+				additional_params = '&mode=bicyclingkey&units=imperial&key=' + ENV['GOOGLE_MAPS_KEY']
+				uri = URI(base_url + location_parameters + additional_params)
+				response = Net::HTTP.get(uri)
+				result = JSON.parse(response)
+
+				if result['status'] == 'OK'
+					result['rows'].each_with_index do |row,i|
+						row['elements'].each do |element,j|
+							ride.distance = element['distance']['value'].to_f
+							ride.save
+						end
+					end
+					json_response(ride)
+				else
+					json_response('whoops')
+				end
 			else
 				json_response(ride)
 			end
-		else
-			json_response('why')
+			
 		end
 	end
 end
